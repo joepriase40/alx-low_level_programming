aae22f073e82f7cda4aeed8f39381a60166c2b59
joepriase40 <joepriase40@gmail.com>
2022-12-22 02:53:51 -0800

    Learn your times table

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,10 +1,12 @@
-#include "main.h"/**
-* main - check the code
-*
-* Return: Always 0.
-*/
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-times_table();
-return (0);
+    times_table();
+    return (0);
 }
