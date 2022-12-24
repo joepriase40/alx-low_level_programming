d08e1bbc10b50b0e2e5d6ce56c84b45c624d588b
joepriase40 <joepriase40@gmail.com>
2022-12-23 17:29:06 -0800

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tets function that prints if integer is positive or negative
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
