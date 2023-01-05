fc36c11e47f9ac02e0d49c415fc53bfd1d845328
joepriase40 <joepriase40@gmail.com>
2023-01-05 07:44:00 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
-	}
+        if (*s)
+        {
+                _putchar(*s);
+                s++;
+                _puts_recursion(s);
+        }
 
-	else
-		_putchar('\n');
+        else
+                _putchar('\n');
 }
