ff3eb8bb3f117153b70d07c0f7cd7ba6c6c7cebc
joepriase40 <joepriase40@gmail.com>
2023-01-08 19:33:46 -0800

    Update

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -8,8 +8,8 @@
  */
 int _atoi(char *s)
 {
-int sign = 1, i = 0;
-unsigned int res = 0;
+	int sign = 1, i = 0;
+	unsigned int res = 0;
 
 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
@@ -23,5 +23,5 @@ unsigned int res = 0;
 		i++;
 	}
 	res *= sign;
-return (res);
+	return (res);
 }
--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -22,6 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = '\0';
 		j++;
 	}
-
 	return (dest);
 }
