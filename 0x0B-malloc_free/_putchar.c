bc45f2f04d31fd2e3d699dcfe8c1579b57d0a5b5
joepriase40 <joepriase40@gmail.com>
2023-01-10 13:15:16 -0800

    Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <unistd.h>
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,9 +7,8 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 
 {
-	return (write(1, &c, 1));		
+	return (write(1, &c, 1));
 }
