ed8f7af2fba3b982f7044b4e7eba8be95bc6d9a9
joepriase40 <joepriase40@gmail.com>
2023-01-12 09:49:39 -0800

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -37,7 +37,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		for (i = 0; i < new_size; i++)
 			ptr1[i] = old_ptr[i];
 	}
-	if (new_size > old_size
+	if (new_size > old_size)
 	{
 	for (i = 0; i < old_size; i++)
 		ptr1[i] = old_ptr[i];
